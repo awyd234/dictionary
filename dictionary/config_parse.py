0c09a98ebde01c5f87423e2f473e8fac2f8d4dfb
Qian Hu <everawyd234@gmail.com>
2017-01-06 16:39:55 +0800

    fix the problem cannot find the configure path

--- dictionary/config_parse.py
+++ dictionary/config_parse.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+import os
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+config_path = os.path.join(parent_dir, 'etc/config.ini')
 
 config = configparser.ConfigParser(allow_no_value=True)
-config.read('../etc/config.ini')
+config.read(config_path)
 
 
 def get_value(section, option, default=None):
